be916ad99fdb0a19d9e1e95277c1c19ec2832686
=HADES11 <=hades11@redberry.ge>
2021-07-15 12:12:54 +0400

    upload project

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+<article>
+    <?=$posts   ?>
+</article>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('posts/{post}', function ($slug) {
+
+    $path=file_get_contents(__DIR__ . '/../resources/post/'.$slug.'.html');
+
+    if (! file_exists($path)){
+       redirect('/');
+    }
+    return view('blog', [
+        'posts' =>$path
+    ]);
+})->where('post','[A-z_\-]+');
+
